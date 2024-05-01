5f9c30ae33ccf622dd832a16e4b92539add46fc6
Brandon <brandon.redford@outlook.com>
2024-05-01 06:22:22 -0700

    add routes and data folders

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
+require('dotenv').config()
+
 const express = require('express');
 const app = express()
+const cors = require('cors')
 
+const PORT = process.env.PORT || 8080 
+const { CORS_ORIGIN } = process.env;
+
+app.use(cors({ CORS_ORIGIN }));
 app.use(express.json())
 
-app.listen(8080, () => console.log('Server started'))
+const videosRouter = require('./routes/videos')
+app.use('/videos', videosRouter)
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`)
+    console.log('CTRL + C to stop server')
+})
